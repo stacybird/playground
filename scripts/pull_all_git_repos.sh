0d411255b7dabc765b91c187a0b52363bb90c24e
Stacy Watts <dreamsong@gmail.com>
2014-09-23 10:39:44 -0700

    add script to poll a git dir, try to switch to master, and do a pull

---
+++ scripts/pull_all_git_repos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then 
+    echo usage: $0 directory
+    exit
+fi
+
+STR=`which git`
+if [ "$STR" = "" ]; then
+    echo "Sorry, git needs to be installed first"
+    exit
+fi
+
+cd $1
+for i in $(ls); do
+    cd $i
+    printf "\n"$i"\n"
+    git checkout master
+    git pull
+    cd ../
+done

0040a2d98ada6b916468c9599fd65258c11ad9e5
Stacy Watts <stacy.watts@acquia.com>
2017-08-09 12:43:08 -0700

    add prune stale git repos

---
+++ scripts/prune_stale_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then 
+    echo usage: $0 directory
+    exit
+fi
+
+STR=`which git`
+if [ "$STR" == "" ]; then
+    echo "Sorry, git needs to be installed first"
+    exit
+fi
+
+cd $1
+for i in $(ls); do
+    cd $i
+    printf "\n"$i"\n"
+    git remote prune origin
+    cd ../
+done

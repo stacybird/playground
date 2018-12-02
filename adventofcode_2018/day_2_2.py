ac6bef46313d8313f5b85c0a151a26a12fbe997a
Stacy Watts <dreamsong@gmail.com>
2018-12-01 23:00:18 -0800

    advent of code day 2.2

---
+++ adventofcode_2018/day_2_2.py
@@ -0,0 +1,25 @@
+# Advent of code 2.1
+# Happy Advent!
+with open("day_2_1.txt") as f:
+    string_array = [line.rstrip() for line in f]
+
+temporary_match = ""
+matching_box_chars = ""
+
+def compare(e1, e2):
+    new_string = ""
+    for char1, char2 in zip(e1, e2):
+        if char1 == char2:
+            new_string += char1
+    return new_string
+
+for element in string_array:
+    for e3 in string_array:
+        if element == e3:
+            pass
+        else:
+            temporary_match = compare(element, e3)
+            if len(temporary_match) > len(matching_box_chars):
+                matching_box_chars = temporary_match
+
+print(matching_box_chars)

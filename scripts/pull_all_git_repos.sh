b784b7a7f71ff0a3a37f8a08ec6fe10791ff19a2
Stacy Watts <dreamsong@gmail.com>
2022-10-04 11:22:58 -0700

    remote primary branch agnostic, ignore terraform subfolders locally

--- scripts/pull_all_git_repos.sh
+++ scripts/pull_all_git_repos.sh
@@ -18,10 +18,10 @@ if [ $? == 1 ]; then
 fi
 
 PWD=`pwd`
-for i in $( find $1 -name ".git" | sort | sed 's/....$//' ); do
+for i in $( find $1 -name ".git" | sort | sed 's/....$//' | grep -v ".terraform"); do
     cd $i
     printf "\n"$i"\n"
-    git checkout master |grep -v up-to-date
+    git checkout $( git symbolic-ref refs/remotes/origin/HEAD | cut -d"/" -f4 ) |grep -v up-to-date
     git pull --ff-only |grep -v Already|grep -v Current
 done
 cd $PWD

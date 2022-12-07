d1453633cae8aff32a6447b2d9e1e97257edd9df
Stacy Watts <dreamsong@gmail.com>
2022-12-06 18:40:39 -0800

    day 1 advent of code testing

---
+++ adventofcode_2022/day_1_1.py
@@ -0,0 +1,18 @@
+# Advent of code 1.1
+# Happy Advent!
+with open("day_1_1.txt") as f:
+    array = [line.rstrip() for line in f]
+j = 0
+array_sums = [0]
+array_sums[j] = 0
+for i in array:
+    if i == '':
+        j = j + 1
+        array_sums.insert(j,0)
+    else:
+        array_sums[j] = array_sums[j] + int(i)
+# array_sum = sum(num_array)
+print(array)
+print(array_sums)
+print(max(array_sums))
+print(array_sums.index(max(array_sums)))
